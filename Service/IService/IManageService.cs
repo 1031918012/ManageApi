e844ac26fbc3dcfc1220e58ca9eb9ef977b3bb50
kaiser.ouyang <1031918012@qq.com>
2019-02-14 19:40:37 +0800

    修改

---
+++ Service/IService/IManageService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain;
+
+namespace Service
+{
+    public interface IManageService
+    {
+        bool Add(ManageItem manage);
+        bool Update(ManageItem manage);
+        ManageItem SelectEntity(Guid id);
+        List<ManageItem> SelectList();
+    }
+}
