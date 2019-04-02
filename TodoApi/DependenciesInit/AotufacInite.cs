d60d50ae51f73a245cc3df5081bc8fd2503ee666
郑德龙 <zhdl1562548508@163.com>
2019-04-02 12:34:24 +0800

    1:通过反射注册容器 2:登陆增加校验

---
+++ TodoApi/DependenciesInit/AotufacInite.cs
@@ -0,0 +1,46 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ManageApi.DependenciesInit
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class AotufacInite
+    {
+        /// <summary>
+        /// 通过反射实现自动化容器注册
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceProvider ConvertToAutofac(this IServiceCollection services)
+        {
+            var containerBuilder = new ContainerBuilder();
+            // containerBuilder.RegisterModule<DependenciesInit>();
+            //替换完容器，构造控制器需要的参数，是autofac做的，但是控制器本身是ServiceCollection做的，包括内置的那几个
+            containerBuilder.Populate(services);
+
+            var dataAccess = Assembly.GetExecutingAssembly();
+            var serviceAccess = Assembly.Load("Service");
+            var infrastructureAccess = Assembly.Load("Infrastructure");
+            var RepositoriesAccess = Assembly.Load("Repositories");
+            var DomainAccess = Assembly.Load("Domain");
+
+            containerBuilder.RegisterTypes(dataAccess.GetTypes());
+            containerBuilder.RegisterTypes(serviceAccess.GetTypes()).AsImplementedInterfaces();
+            containerBuilder.RegisterTypes(infrastructureAccess.GetTypes()).AsImplementedInterfaces();
+            containerBuilder.RegisterTypes(RepositoriesAccess.GetTypes()).AsImplementedInterfaces();
+            containerBuilder.RegisterTypes(DomainAccess.GetTypes()).AsImplementedInterfaces();
+
+            var container = containerBuilder.Build();
+
+            return new AutofacServiceProvider(container);
+        }
+    }
+}
--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -1,18 +1,13 @@
-﻿using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using Domain;
-using Infrastructure.Filter;
+﻿using Infrastructure.Filter;
 using ManageApi;
 using ManageApi.DependenciesInit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Repositories;
-using Service;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
@@ -46,7 +41,8 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(o=> {
+            services.AddMvc(o =>
+            {
                 o.Filters.Add(typeof(CustomExceptionFilterAttribute));
             });
             services.AddCors(options =>
@@ -58,16 +54,6 @@ namespace TodoApi
                     //允许所有来源，允许所有HTTP方法，允许所有作者的请求标头
                 });
             });
-            services.AddScoped<RayPIToken>();
-            services.AddScoped<IPeopleService, PeopleService>();
-            services.AddScoped<IPeopleRepository, PeopleRepository>();
-            services.AddScoped<IUserService, UserService>();
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<IManageService, ManageService>();
-            services.AddScoped<IManageRepository, ManageRepository>();
-            services.AddScoped<ISalaryUnitOfWork, SalaryUnitOfWork>();
-            services.AddScoped<IRepositories<IManage>, EFRepositories<IManage>>();
-            services.AddSingleton<IMemoryCache>(new MemoryCache(new MemoryCacheOptions()));
             services.AddDbContextPool<ManageContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("ManageConnectionStrings"), b => b.MigrationsAssembly("Repositories"));
@@ -103,21 +89,7 @@ namespace TodoApi
                 options.AddPolicy("Admin", policy => policy.RequireClaim("Admin").Build());
             });
             #region autofac
-            var containerBuilder = new ContainerBuilder();
-            containerBuilder.RegisterModule<DependenciesInit>();
-            //替换完容器，构造控制器需要的参数，是autofac做的，但是控制器本身是ServiceCollection做的，包括内置的那几个
-            containerBuilder.Populate(services);
-            //services.AddScoped<IPeopleService, PeopleService>();
-            //services.AddScoped<IPeopleRepository, PeopleRepository>();
-            //services.AddScoped<IUserService, UserService>();
-            //services.AddScoped<IUserRepository, UserRepository>();
-            //services.AddScoped<IManageService, ManageService>();
-            //services.AddScoped<IManageRepository, ManageRepository>();
-            //services.AddScoped<ISalaryUnitOfWork, SalaryUnitOfWork>();
-            //services.AddScoped<IRepositories<IManage>, EFRepositories<IManage>>();
-            //services.AddSingleton<IMemoryCache>(new MemoryCache(new MemoryCacheOptions()));
-            var container = containerBuilder.Build();
-            return new AutofacServiceProvider(container);
+            return AotufacInite.ConvertToAutofac(services);
             #endregion
         }
         /// <summary>
