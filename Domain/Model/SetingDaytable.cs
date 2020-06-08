bce22278fd44691d81ccfaa82751384b9d1ef2b3
kaiser.ouyang <1031918012@qq.com>
2020-06-08 17:40:19 +0800

    1

--- Domain/Model/SetingDaytable.cs
+++ Domain/Model/SetingDaytable.cs
@@ -33,5 +33,37 @@ namespace Domain
         /// 所属客户编号
         /// </summary>
         public string CompanyId { get; set; }
+        /// <summary>
+        /// 班次名称
+        /// </summary>
+        public string ShiftName { get; set; }
+        /// <summary>
+        /// 打卡时段
+        /// </summary>
+        public string ShiftDetails { get; set; }
+        /// <summary>
+        /// 是否存在豁免时间(定义：迟到多少分钟不算，早退多少分钟不算)
+        /// </summary>
+        public bool IsExemption { get; set; }
+        /// <summary>
+        /// 豁免迟到分钟数
+        /// </summary>
+        public int LateMinutes { get; set; }
+        /// <summary>
+        /// 豁免早退分钟数
+        /// </summary>
+        public int EarlyLeaveMinutes { get; set; }
+        /// <summary>
+        /// 是否存在弹性时间
+        /// </summary>
+        public bool IsFlexible { get; set; }
+        /// <summary>
+        /// 早到早走
+        /// </summary>
+        public int EarlyFlexible { get; set; }
+        /// <summary>
+        /// 晚到晚走
+        /// </summary>
+        public int LateFlexible { get; set; }
     }
 }
---
+++ TodoApi/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ManageApi.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class HomeController : ControllerBase
+    {
+
+    }
+}
